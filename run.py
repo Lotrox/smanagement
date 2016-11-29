7db41cd762ccbe8ca97ab1abae48a6037da0ea81
Daniel Martínez Caballero <dmc00024@red.ujaen.es>
2016-11-29 22:42:14 +0100

    Origen de los tiempos

---
+++ run.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Daniel Martinez Caballero
+# Description: API REST Framework Bottle.
+
+from bottle import Bottle, route, run, request, HTTPResponse, response
+from gevent import monkey; monkey.patch_all()
+
+
+run(host='0.0.0.0', port=8080, reloader=False, server='gevent')
+
