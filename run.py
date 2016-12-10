3e5056bc3f5f8908c9d3cbc5bedd74edbbae99d8
Daniel Martínez Caballero <dmc00024@red.ujaen.es>
2016-12-11 00:11:47 +0100

    Manejador de señal de apagado para detención del servicio.

--- run.py
+++ run.py
@@ -6,7 +6,14 @@
 from project import app
 from bottle import run, request, debug
 from gevent import monkey; monkey.patch_all()
+import signal, sys
 
+# Manejador necesario para realizar la parada del servicio mediante señal de apagado.
+def signal_handler(signal, frame):
+        print('Stopping Signal_Handler')
+        sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
 
 if __name__ == '__main__':
 	run(app, reloader=True, host='0.0.0.0', port=8081, server='gevent')
