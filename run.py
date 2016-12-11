4891ebf78eac0a63c62d81d1dbe2324ca34bcaf8
Daniel Martínez Caballero <dmc00024@red.ujaen.es>
2016-12-11 15:55:39 +0100

    Añadida comunicación por SSL.

--- run.py
+++ run.py
@@ -4,9 +4,10 @@
 # Description: API REST Framework Bottle.
 
 from project import app
-from bottle import run, request, debug
+from bottle import run, request, debug, ServerAdapter
 from gevent import monkey; monkey.patch_all()
-import signal, sys
+import signal, sys, os
+
 
 # Manejador necesario para realizar la parada del servicio mediante señal de apagado.
 def signal_handler(signal, frame):
@@ -15,8 +16,22 @@ def signal_handler(signal, frame):
 
 signal.signal(signal.SIGINT, signal_handler)
 
-if __name__ == '__main__':
-	run(app, reloader=True, host='0.0.0.0', port=8081, server='gevent')
 
+if __name__ == '__main__':
+	# Soporte de SSL junto con servidor Gevent. Ref: https://github.com/bottlepy/bottle/issues/436
+	class SecureGeventServer(ServerAdapter):
+		def run(self, handler):
+       			from gevent import wsgi, pywsgi, local
+		        import threading
+		        _lcth = threading.local()
+		        if not isinstance(_lcth, local.local):
+				msg = "Bottle requires gevent.monkey.patch_all() (before import)"
+		                raise RuntimeError(msg)
+		        if not self.options.get('fast'): wsgi = pywsgi
+		        log = None if self.quiet else 'default'
+		        wsgi.WSGIServer((self.host, self.port), handler, log=log, keyfile='/opt/smanagement/api-rest/key.pem', certfile='/opt/smanagement/api-rest/cert.pem').serve_forever()
+
+	port = int(os.environ.get("PORT", 8091))
+	run(app, host='0.0.0.0', port=port, server=SecureGeventServer)
 
 
