17e65bdc28536ecbc2acd56342f982c3913011a8
Daniel Martínez Caballero <dmc00024@red.ujaen.es>
2016-12-16 19:19:40 +0100

    -Controlador Wireless implementado. -Método para obtener la lista actual de clientes WiFi.

---
+++ project/controllers/wireless.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Daniel Martinez Caballero
+# Description: Wireless Controller.
+
+from project import app
+from bottle import request, HTTPResponse
+import os
+
+name = '/' + os.path.splitext(os.path.basename(__file__))[0]
+
+
+@app.route(name + '/clients', method='GET')
+def clients():
+	"""
+        Obtener la lista de dispositivos conectados al punto de acceso WiFi.
+        Se realiza una intersección de MAC entre los servicios de dnsmasq y hostapd,
+        de este modo conseguimos además la dirección IP asignada y el nombre del dispositivo.
+        """
+
+        cmd = "echo \"$(journalctl -eu dnsmasq | grep \"$(sudo hostapd_cli all_sta | grep dot11RSNAStatsSTAAddress | cut -d \"=\" -f2 | cut -d \" \" -f1)\" | grep DHCPACK | cut -c61-)\" > /tmp/ap-clients"
+        os.system(cmd)
+        output = str(os.popen("awk '!a[$0]++' /tmp/ap-clients").read())
+        raise HTTPResponse(status=200, body=output.replace('\n', '<br />'))
